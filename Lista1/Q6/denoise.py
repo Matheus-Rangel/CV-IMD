860d19f274d4922a1d78e00416c8c5bd23fb9bf1
matheus <matheus_rangel@ufrn.edu.br>
2019-02-25 10:18:15 -0300

    Denoising by mean

--- Lista1/Q6/denoise.py
+++ Lista1/Q6/denoise.py
@@ -1,27 +1,31 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import os
+
 def denoise1(imgs):
     img = cv2.fastNlMeansDenoisingMulti(imgs, 8, 1)
     return img
 
 def denoise2(imgs):
     img_out = imgs[0] * 0
+    img_out = img_out.astype('int')
     for img in imgs:
         img_out += img
+    img_out = np.floor_divide(img_out, len(imgs)) 
     return img_out
+
 def colormap():
-    cdict = {'red':((0.0, 0.0, 0.0),
-                   (0.5, 0.0, 1.0),
-                   (1.0, 0.1, 1.0)),
+    cdict = {'red': ((0.0, 0.0, 0.0),
+                    (1.0, 0.7, 0.7)),
 
-         'green': ((0.0, 0.0, 0.0),
-                   (1.0, 0.0, 0.0)),
+         'green':   ((0.0, 0.0, 0.0),
+                    (0.5, 0.0, 0.0),
+                    (1.0, 0.6, 0.6)),
 
-         'blue':  ((0.0, 0.0, 0.1),
-                   (0.5, 1.0, 0.0),
-                   (1.0, 0.0, 0.0))
+         'blue':  ((0.0, 0.0, 0.0),
+                   (1.0, 1.0, 1.0))
         }
     return LinearSegmentedColormap('custom_cmap', cdict)
 if __name__ == "__main__":
@@ -30,4 +34,4 @@ if __name__ == "__main__":
         imgs.append(cv2.imread('img/' + img, 0))
     cv2.imwrite('denoise/denoise1.jpg', denoise1(imgs))
     cv2.imwrite('denoise/denoise2.jpg', denoise2(imgs))
-    plt.imsave('denoise/denoise_colored.jpg', denoise1(imgs), cmap='hot')
+    plt.imsave('denoise/denoise_colored.jpg', denoise2(imgs)*5, cmap=colormap())
